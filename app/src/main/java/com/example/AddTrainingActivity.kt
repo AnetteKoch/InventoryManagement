eaf93ce33a951229a38edaa20df7fa38c2b2da71
AnetteKoch <AnetteKoch@users.noreply.github.com>
2020-09-09 21:43:31 +0200

    Datedisplay

--- app/src/main/java/com/example/AddTrainingActivity.kt
+++ app/src/main/java/com/example/AddTrainingActivity.kt
@@ -2,16 +2,20 @@ package com.example
 
 import android.content.Context
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.add_training.*
 
 class AddTrainingActivity: AppCompatActivity() {
 
+    lateinit var date : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_training)
 
-
+        date = findViewById(R.id.date)
+        date.text = MainActivity.dateView.text
 
 
     }
